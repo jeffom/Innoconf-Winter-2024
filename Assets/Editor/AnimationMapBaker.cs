6a2906892aec319e24ce0764d70b858c719e5398
Jefferson Kaneta <jefferson.kaneta@innogames.com>
2024-04-30 11:26:38 +0200

    -update state and WIP

--- Assets/Editor/AnimationMapBaker.cs
+++ Assets/Editor/AnimationMapBaker.cs
@@ -1,7 +1,15 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
+using Codice.CM.Common;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Pool;
+using UnityEngine.Rendering;
+using VatBaker;
+using VatBaker.Editor;
 
 namespace Editor
 {
@@ -19,71 +27,71 @@ namespace Editor
         }
 
         //Return the list of baked textures containing the position, rotation and scale for animations
-        public Dictionary<string, Texture2D> BakeAnimationToTexture(GameObject target, AnimationClip anim)
+        public (Dictionary<SkinnedMeshRenderer, Texture2D>, Dictionary<SkinnedMeshRenderer, Texture2D> )
+            BakeAnimationToTexture(GameObject target, AnimationClip anim, bool packTextures, Space space = Space.Self)
         {
             Dictionary<string, Texture2D> bakedTextures = new Dictionary<string, Texture2D>();
-            int height = (int)(anim.length * anim.frameRate) + 1;
+            int height = Mathf.FloorToInt(anim.length * anim.frameRate) + 1;
             var bakedMesh = new Mesh();
             float dt = 1f / anim.frameRate;
-            Vector3[] v = target.GetComponentsInChildren<SkinnedMeshRenderer>()
-                .SelectMany(mf => mf.sharedMesh.vertices)
-                .ToArray();
-
-            Dictionary<Material, List<SkinnedMeshRenderer>> renderDict = new Dictionary<Material, List<SkinnedMeshRenderer>>();
             Dictionary<SkinnedMeshRenderer, Texture2D> meshDict = new Dictionary<SkinnedMeshRenderer, Texture2D>();
-
+            Dictionary<SkinnedMeshRenderer, Texture2D>
+                meshNormalDict = new Dictionary<SkinnedMeshRenderer, Texture2D>();
+            using var poolVtx0 = ListPool<Vector3>.Get(out var tmpVertexList);
+            using var poolVtx1 = ListPool<Vector3>.Get(out var localVertices);
+            using var poolNorm0 = ListPool<Vector3>.Get(out var tmpNormalList);
+            using var poolNorm1 = ListPool<Vector3>.Get(out var localNormals);
+            
             var meshRenderers = target.gameObject.GetComponentsInChildren<SkinnedMeshRenderer>();
             foreach (var mr in meshRenderers)
             {
-                if (!renderDict.ContainsKey(mr.sharedMaterial))
-                {
-                    var list = new List<SkinnedMeshRenderer>();
-                    list.Add(mr);
-                    renderDict.Add(mr.sharedMaterial, list);
-                }
-                else
-                {
-                    renderDict[mr.sharedMaterial].Add(mr);
-                }
-            }
-
-            foreach (var entry in renderDict)
-            {
-                var renderers = entry.Value;
-                List<Vector3> vertices = new List<Vector3>();
-                foreach (var r in renderers)
-                {
-                    r.BakeMesh(bakedMesh);
-                    vertices.AddRange(bakedMesh.vertices);
-                }
-
-                var posTex = CreateBakeTexture(vertices.Count, height, anim.isLooping);
+                tmpVertexList.Clear();
+                tmpNormalList.Clear();
+                localVertices.Clear();
+                localNormals.Clear();
+                using var tranScope = TransformCacheScope.ResetScope(mr.transform);
+                bakedMesh = new Mesh();
 
                 for (var y = 0; y < height; y++)
                 {
-                    anim.SampleAnimation(target.gameObject, y * dt);
-                    vertices = new List<Vector3>();
-                    foreach (var r in renderers)
-                    {
-                        r.BakeMesh(bakedMesh);
-                        vertices.AddRange(bakedMesh.vertices);
-                    }
+                    anim.SampleAnimation(target, y*dt);
+                    mr.BakeMesh(bakedMesh);
 
-                    for (var x = 0; x < vertices.Count; x++)
-                    {
-                        posTex.SetPixel(x, y, new Color(vertices[x].x, vertices[x].y, vertices[x].z));
-                    }
+                    bakedMesh.GetVertices(tmpVertexList);
+                    bakedMesh.GetNormals(tmpNormalList);
+
+                    localVertices.AddRange(tmpVertexList);
+                    localNormals.AddRange(tmpNormalList);
                 }
 
-                bakedTextures.Add(entry.Key.name, posTex);
+                var trans = target.transform;
+                var (vertices, normals) = space switch
+                {
+                    Space.Self => (
+                        localVertices.Select(vtx => trans.InverseTransformPoint(vtx)),
+                        localNormals.Select(norm => trans.InverseTransformDirection(norm))
+                    ),
+                    Space.World => (localVertices, localNormals),
+
+                    _ => throw new ArgumentOutOfRangeException(nameof(space), space, null)
+                };
+
+                meshDict.TryAdd(mr, CreateBakeTexture(mr.sharedMesh.vertexCount, height, anim.isLooping));
+                meshNormalDict.TryAdd(mr, CreateBakeTexture(mr.sharedMesh.vertexCount, height, anim.isLooping));
+
+                meshDict[mr].SetPixels(ListToColorArray(vertices));
+                meshNormalDict[mr].SetPixels(ListToColorArray(normals));
             }
 
-            return bakedTextures;
+            static Color[] ListToColorArray(IEnumerable<Vector3> list) =>
+                list.Select(v3 => new Color(v3.x, v3.y, v3.z)).ToArray();
+
+            return (meshDict, meshNormalDict);
         }
 
         Texture2D CreateBakeTexture(int w, int h, bool isLooping)
         {
-            return new Texture2D(Mathf.ClosestPowerOfTwo(w), Mathf.ClosestPowerOfTwo(h), TextureFormat.RGBAHalf, false)
+            return new Texture2D(w, h, TextureFormat.RGBAHalf, false)
             {
                 wrapModeU = TextureWrapMode.Clamp,
                 wrapModeV = isLooping ? TextureWrapMode.Repeat : TextureWrapMode.Clamp,
@@ -112,9 +120,42 @@ namespace Editor
             return uv;
         }
 
-        public static float Remap(float value, float from1, float to1, float from2, float to2)
+        static float Remap(float value, float from1, float to1, float from2, float to2)
         {
             return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
         }
-    }
+        
+        public static readonly int MainTex = Shader.PropertyToID("_MainTex");
+        public static readonly int NormalTex = Shader.PropertyToID("_NormalTex");
+        private static readonly int BaseShaderBumpMap = Shader.PropertyToID("_BumpMap");
+        public Material GenerateMaterial(SkinnedMeshRenderer skin, Texture2D bakedAnim, Texture2D bakedNormal, string materialName, Shader shader, float fps, float animLength)
+        {   
+            var mat = new Material(shader)
+            {
+                enableInstancing = true
+            };
+
+           
+            mat.SetTexture(MainTex, skin.sharedMaterial.mainTexture);
+            var normalTex = skin.sharedMaterial.GetTexture(BaseShaderBumpMap);
+            if (normalTex != null)
+            {
+                mat.SetTexture(NormalTex, normalTex);
+            }
+
+            mat.name = materialName;
+            mat.SetTexture(VatShaderProperty.VatPositionTex, bakedAnim);
+            mat.SetTexture(VatShaderProperty.VatNormalTex, bakedNormal);
+            mat.SetFloat(VatShaderProperty.VatAnimFps, fps);
+            mat.SetFloat(VatShaderProperty.VatAnimLength, animLength);
+
+            return mat;
+        }
+        
+        static readonly string InvalidChars = new string(Path.GetInvalidPathChars());
+        static string ReplaceInvalidPathChar(string path)
+        {
+            return Regex.Replace(path, $"[{InvalidChars}]", "_");
+        }
+}
 }
--- Assets/Editor/MyCustomEditor.cs
+++ Assets/Editor/MyCustomEditor.cs
@@ -1,19 +1,24 @@
 using System.ComponentModel.Composition.Hosting;
 using System.IO;
+using System.Text.RegularExpressions;
 using Editor;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEditor.UIElements;
+using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class MyCustomEditor : EditorWindow
 {
-    [SerializeField]
-    private VisualTreeAsset m_VisualTreeAsset = default;
+    [SerializeField] private VisualTreeAsset m_VisualTreeAsset = default;
 
     private Animator m_selectedAnimator = null;
-    
+    private string m_outputPath = string.Empty;
+    private Shader m_selectedShader = null;
+    private bool m_packAnimations = false;
+    private Space m_animationSpace = Space.Self;
+
     [MenuItem("Window/UI Toolkit/MyCustomEditor")]
     public static void ShowExample()
     {
@@ -28,9 +33,9 @@ public class MyCustomEditor : EditorWindow
 
         m_VisualTreeAsset.CloneTree(root);
 
-        var objectField = root.Q<ObjectField>("ObjectField");
+        var objectField = root.Q<ObjectField>("AnimatorObjectField");
         objectField.objectType = typeof(Animator);
-        objectField.RegisterValueChangedCallback( evt =>
+        objectField.RegisterValueChangedCallback(evt =>
         {
             Animator animator = evt.newValue as Animator;
             m_selectedAnimator = animator;
@@ -45,32 +50,86 @@ public class MyCustomEditor : EditorWindow
             }
         });
 
-        var bakeButton = root.Q<Button>("BakeButton");
-        bakeButton.clicked += (() =>
+        var multipleAnimationsCheckbox = root.Q<Toggle>("PackAnimations");
+        multipleAnimationsCheckbox.RegisterValueChangedCallback(evt =>
         {
-            if (m_selectedAnimator == null)
-                return;
-            
-            AnimationMapBaker baker = new AnimationMapBaker();
-            var animations = AnimationMapBaker.GetAllAnimations(m_selectedAnimator);
-            var clip = animations[0];
-            var outputTextures = baker.BakeAnimationToTexture(m_selectedAnimator.gameObject, clip);
-            foreach (var entry in outputTextures)
-            {
-                var texture = entry.Value;
-                var folderPath = Path.Combine("Assets/BakedTextures/", "Output");
-                if (!AssetDatabase.IsValidFolder(folderPath))
-                {
-                    AssetDatabase.CreateFolder("Assets/BakedTextures/", "Output");
-                }
-                
-                var data = entry.Value;
-                var animMap = new Texture2D(data.width, data.height, TextureFormat.RGBAHalf, false);
-                animMap.LoadRawTextureData(data.GetRawTextureData());
-                AssetDatabase.CreateAsset(data, Path.Combine(folderPath, entry.Key + ".asset"));
-            }
-            
-            
+            bool newValue = evt.newValue;
+            m_packAnimations = newValue;
         });
+
+        var shaderObjectField = root.Q<ObjectField>("ShaderObjectField");
+        shaderObjectField.objectType = typeof(Shader);
+        shaderObjectField.RegisterValueChangedCallback(evt =>
+        {
+            Shader shader = evt.newValue as Shader;
+            m_selectedShader = shader;
+        });
+
+        var bakeButton = root.Q<Button>("BakeButton");
+        bakeButton.clicked += BakeTexture;
+
+        var outputTextField = root.Q<TextField>("FolderOutputTextfield");
+        outputTextField.textEdition.placeholder = "Assets/BakedTextures/";
+        outputTextField.textEdition.isReadOnly = true;
+
+        var selectOutputFolderButton = root.Q<Button>("SelectOutputFolderButton");
+        m_outputPath = outputTextField.textEdition.placeholder;
+        selectOutputFolderButton.clicked += () =>
+        {
+            var pattern = @"\/Assets\/";
+            string outputPath = EditorUtility.OpenFolderPanel("Select the output", Application.dataPath, "");
+            if (Regex.Match(outputPath, pattern).Success)
+            {
+                outputTextField.textEdition.placeholder = outputPath.Substring(outputPath.IndexOf("Assets"));
+                m_outputPath = outputTextField.textEdition.placeholder;
+            }
+        };
+
+        var animationSpaceSelector = root.Q<EnumField>("AnimationSpaceEnum");
+        animationSpaceSelector.RegisterValueChangedCallback(evt =>
+        {
+            var newValue = (Space)evt.newValue;
+            m_animationSpace = newValue;
+        });
+    }
+
+    void BakeTexture()
+    {
+        if (m_selectedAnimator == null)
+            return;
+
+        AnimationMapBaker baker = new AnimationMapBaker();
+        var animations = AnimationMapBaker.GetAllAnimations(m_selectedAnimator);
+        var clip = animations[0];
+        var (outputTextures, normalTextures) = baker.BakeAnimationToTexture(m_selectedAnimator.gameObject, clip,
+            m_packAnimations, m_animationSpace);
+        int count = 0;
+        foreach (var entry in outputTextures)
+        {
+            var texture = entry.Value;
+            var folderPath = Path.Combine(m_outputPath, "Output");
+            if (!AssetDatabase.IsValidFolder(folderPath))
+            {
+                AssetDatabase.CreateFolder(m_outputPath, "Output");
+            }
+
+            if (!AssetDatabase.IsValidFolder(Path.Combine(folderPath, "Material")))
+            {
+                AssetDatabase.CreateFolder(folderPath, "Material");
+            }
+
+            var data = entry.Value;
+            var animMap = new Texture2D(data.width, data.height, TextureFormat.RGBAHalf, false);
+            animMap.LoadRawTextureData(data.GetRawTextureData());
+            AssetDatabase.CreateAsset(data, Path.Combine(folderPath, entry.Key.name + (count) + ".asset"));
+            var normalData = normalTextures[entry.Key];
+            AssetDatabase.CreateAsset(normalData,
+                Path.Combine(folderPath, entry.Key.name + (count++) + "_normal.asset"));
+
+            var material = baker.GenerateMaterial(entry.Key, data, normalData, entry.Key.name, m_selectedShader,
+                clip.frameRate, clip.length);
+            AssetDatabase.CreateAsset(material,
+                Path.Combine(Path.Combine(folderPath, "Material"), material.name + ".mat"));
+        }
     }
 }
